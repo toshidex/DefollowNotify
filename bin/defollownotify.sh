81bd49fd82a404b8f7574ddbcf71462c9a7b7c4e
Toshidex <lightuono@gmail.com>
2012-03-30 16:43:45 +0200

    Minor Fix

--- bin/defollownotify.sh
+++ bin/defollownotify.sh
@@ -8,16 +8,16 @@
 # @License : GNU AGPL v3 http://www.gnu.org/licenses/agpl.html
 ###############################################################################
 
-DFN_RC="/usr/local/src/defollownotify/defollownotify.rcs"
+DFN_RC="/usr/local/src/defollownotify/defollownotify.rc"
 
 
 load_config() {
 
-	[[ -f "$DFN_RC" ]] && . "$DFN_RC" || echo -e "\ndefollownotify.rc: File not found!\n $(exit)"
+	[[ -f "$DFN_RC" ]] && . "$DFN_RC" || echo -e "\n defollownotify.rc: File not found!\n $(exit)"
 	
-	[[ "$oauth_consumer_key" == "" ]] && echo -e "\nThe [ oauth File not found!\n $(exit)"
-        [[ "$oauth_consumer_secret" == "" ]] && echo -e "\ndefollownotify.rc: File not found!\n $(exit)"
-
+	[[ "$oauth_consumer_key" == "" ]] && echo -e "\n The variable [ oauth_consumer_key ] not found!\n $(exit)"
+        [[ "$oauth_consumer_secret" == "" ]] && echo -e "\n The variable [ oauth_consumer_secret ] not found!\n $(exit)"
+	[[ "$USERNAME" == "" ]] && echo -e "\n You have not insert an account Twitter"
 
 }
 
