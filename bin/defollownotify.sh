d6400def0c3b4e33fda80ab1783ff8a35287dffe
Toshidex <lightuono@gmail.com>
2012-03-30 16:17:23 +0200

    create function load_config and fix install.sh

--- bin/defollownotify.sh
+++ bin/defollownotify.sh
@@ -8,5 +8,17 @@
 # @License : GNU AGPL v3 http://www.gnu.org/licenses/agpl.html
 ###############################################################################
 
+DFN_RC="/usr/local/src/defollownotify/defollownotify.rcs"
 
 
+load_config() {
+
+	[[ -f "$DFN_RC" ]] && . "$DFN_RC" || echo -e "\ndefollownotify.rc: File not found!\n $(exit)"
+	
+	[[ "$oauth_consumer_key" == "" ]] && echo -e "\nThe [ oauth File not found!\n $(exit)"
+        [[ "$oauth_consumer_secret" == "" ]] && echo -e "\ndefollownotify.rc: File not found!\n $(exit)"
+
+
+}
+
+load_config
--- install.sh
+++ install.sh
@@ -11,30 +11,39 @@
 
 # Init Variables
 check_user=""
+check_dep=0
+declare -A dep
 dep=(
 	["bash"]="$(which bash 2> /dev/null)"
-	["curl"])"$(which curl 2> /dev/null)")
+	["curl"]="$(which curl 2> /dev/null)")
+install_home="/usr/local"
 
 #End Init Variables
 
-echo -e "* Checking user's privileges...\n"
+echo -e "\n* Installation Defollow Notify -"
+
+
+echo -e "\n* Checking user's privileges..."
 
 if [ $(id -u) -ne 0 ]; then
-	echo "	ERROR: User $(whoami) is not root, and does not have sudo privileges"
+	echo -e "\n* ERROR: User $(whoami) is not root, and does not have sudo privileges"
 	exit 1
-else
-	while [ -z $check_user ]; do
-		echo "	Write your exact username: "
-		read username
-		id -u -r $usernmae &> /dev/null
-		
-		if [ $? -eq 0 ]; then
-			check_user=1
-		else
-			echo "	Username don't exists! Please insert a valid username."
-		fi
-	done
+#else
+#	while [ -z $check_user ]; do
+#		echo "	Write your exact username: "
+#		read username
+#		id -u -r $username &> /dev/null
+#		
+#		if [ $? -eq 0 ]; then
+#			check_user=1
+#		else
+#			echo "	Username don't exists! Please insert a valid username."
+#		fi
+#	done
 fi
+
+echo -e "\n* Write your username Twitter: "
+read username
 
 echo -e "\n* Checking dependencies..."
 
@@ -48,5 +57,34 @@ for key in "bash" "curl"; do
 	fi
 done
 
+if [ $check_dep -eq 1 ]; then
+	echo -e "\n* ERROR: You have to install [ $dep_install ] before to continue."
+
+	exit 1
+fi
 
 
+echo -e "\n* Installing in progress.."
+
+#install_home="$(awk -F: -v v="$username" '{ if ( $1 == v ) print $6 }' /etc/passwd)"
+
+if [ ! -d $install_home/src/defollownotify ]; then
+	mkdir $install_home/src/defollownotify
+	echo -e "\n	- Create directory [ defollownotify ] inside $install_home/src"
+	cp conf/defollownotify.rc $install_home/src/defollownotify
+	echo -e "\n	- Copy file [ defollownotify.rc ] inside $install_home/src/defollownotify"
+	cp -fr lib $install_home/src/defollownotify
+	echo -e "\n	- Copy directory [ lib ] inside $install_home/src/defollownotify"
+	cp bin/defollownotify.sh $install_home/bin/defollownotify
+	echo -e "\n	- Copy file execute [ defollownotify.sh ] inside /usr/local/bin"
+	chmod +x $install_home/bin/defollownotify
+	echo -e "\n	- Change permissions at the file [ defollownotify.sh ] inside /usr/local/bin"
+	echo "USERNAME=$username" >> $install_home/src/defollownotify/defollownotify.rc
+	echo "HOMEDIR=$install_home/src/defollownotify" >> $install_home/src/defollownotify/defollownotify.rc
+
+	echo -e "\n* Installation complete!"
+	exit 0
+else
+	echo -e "\n* You have already installed Defollow Notify!"
+	exit 0
+fi
