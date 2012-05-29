b5575b402df97f427a89b8825f5ec2b4cf6e4b88
toshidex <lightuono@gmail.com>
2012-05-29 17:44:22 +0200

    Change version

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 ###############################################################################
 # @Author : Ennio Giliberto aka Lightuono / Toshidex
 # @Name : Defollow Notify
-# @Version: 0.0.3
+# @Version: 0.0.4
 # @Copyright : 2012
 # @Site : http://www.toshidex.org
 # @License : GNU AGPL v3 http://www.gnu.org/licenses/agpl.html
